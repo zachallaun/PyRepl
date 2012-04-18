84a5b48b42feab9d7882a098ab760a67ab8dad2a
Zachary Allaun <zach.allaun@gmail.com>
2012-04-17 23:40:35 -0400

    caja sandboxing and tutorial element

--- coffee/client.coffee
+++ coffee/client.coffee
@@ -1,9 +1,51 @@
 $ ->
-  class PyWatcher
-    act: (c, r, o) ->
-      console.log c, r, o
+  class Tutor
+    constructor: (@exercises) ->
+      @current = @next()
+      @completed = []
+      caja.initialize cajaServer: 'https://caja.appspot.com', debug: true
 
-  watcher = new PyWatcher()
+    next: ->
+      fn = @exercises[0]
+      @exercises = @exercises[1..]
+      fn
+
+    watch: (c, r, o) =>
+      caja.load undefined, undefined, (frame) =>
+        frame.code(@current.url, 'application/javascript', @current.fn)
+              .api(code: c, result: r, output: o)
+              .run (fn) =>
+                @doTest fn
+
+    doTest: (testfn) ->
+      if testfn()
+        console.log testfn()
+      else
+        console.log "GREAT FAILURE"
+
+  testFiles = ["file://localhost/Users/ggzach/Dropbox/" +
+                  "projects/python27/pyrepl/js/test.js"]
+
+  fnOrTrue = (fn) ->
+    if fn is not "" then fn else "function(){return true;}"
+
+  parseFn = (url) ->
+    fn = null
+    $.ajax
+      url: url
+      success: (data) ->
+        fn = data
+      async: false
+    fn
+
+  exObj =
+    task: "This is the first task."
+    url: testFiles[0]
+
+  exObj.fn = parseFn exObj.url
+
+  exercises = [exObj]
+  tutor = new Tutor(exercises)
 
   $("#python-runtime").on 'load', (event) ->
-    PyREPL watcher.act
+    PyREPL tutor.watch
--- js/client.js
+++ js/client.js
@@ -1,21 +1,81 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   $(function() {
-    var PyWatcher, watcher;
-    PyWatcher = (function() {
+    var Tutor, exObj, exercises, fnOrTrue, parseFn, testFiles, tutor;
+    Tutor = (function() {
 
-      function PyWatcher() {}
+      function Tutor(exercises) {
+        this.exercises = exercises;
+        this.watch = __bind(this.watch, this);
+        this.current = this.next();
+        this.completed = [];
+        caja.initialize({
+          cajaServer: 'https://caja.appspot.com',
+          debug: true
+        });
+      }
 
-      PyWatcher.prototype.act = function(c, r, o) {
-        return console.log(c, r, o);
+      Tutor.prototype.next = function() {
+        var fn;
+        fn = this.exercises[0];
+        this.exercises = this.exercises.slice(1);
+        return fn;
       };
 
-      return PyWatcher;
+      Tutor.prototype.watch = function(c, r, o) {
+        var _this = this;
+        return caja.load(void 0, void 0, function(frame) {
+          return frame.code(_this.current.url, 'application/javascript', _this.current.fn).api({
+            code: c,
+            result: r,
+            output: o
+          }).run(function(fn) {
+            return _this.doTest(fn);
+          });
+        });
+      };
+
+      Tutor.prototype.doTest = function(testfn) {
+        if (testfn()) {
+          return console.log(testfn());
+        } else {
+          return console.log("GREAT FAILURE");
+        }
+      };
+
+      return Tutor;
 
     })();
-    watcher = new PyWatcher();
+    testFiles = ["file://localhost/Users/ggzach/Dropbox/" + "projects/python27/pyrepl/js/test.js"];
+    fnOrTrue = function(fn) {
+      if (fn === !"") {
+        return fn;
+      } else {
+        return "function(){return true;}";
+      }
+    };
+    parseFn = function(url) {
+      var fn;
+      fn = null;
+      $.ajax({
+        url: url,
+        success: function(data) {
+          return fn = data;
+        },
+        async: false
+      });
+      return fn;
+    };
+    exObj = {
+      task: "This is the first task.",
+      url: testFiles[0]
+    };
+    exObj.fn = parseFn(exObj.url);
+    exercises = [exObj];
+    tutor = new Tutor(exercises);
     return $("#python-runtime").on('load', function(event) {
-      return PyREPL(watcher.act);
+      return PyREPL(tutor.watch);
     });
   });
 
