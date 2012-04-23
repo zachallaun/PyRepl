2d76f43c9000924742594fbde31b3823e7583c67
Zachary Allaun <zach.allaun@gmail.com>
2012-04-23 00:40:46 -0400

    Setup simple Lesson model with sqlalchemy

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import os
+BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 
 from flask import Flask
 app = Flask(__name__)
 
 from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_PATH, 'db/test.db')
+db = SQLAlchemy(app)
+
+class Lesson(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  task = db.Column(db.String())
+  test = db.Column(db.Unicode())
 
 @app.route('/')
 def index():
